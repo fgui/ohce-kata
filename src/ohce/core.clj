0f8af01ac8a96a9f8cb70da3218f8aaf75105a49
francesc <francesc.guillen@gmail.com>
2016-06-22 23:28:40 +0200

    refactor greeting

--- src/ohce/core.clj
+++ src/ohce/core.clj
@@ -3,15 +3,19 @@
 (defn get-hour-now []
   (.get (java.util.Calendar/getInstance) java.util.Calendar/HOUR_OF_DAY))
 
-(defn greeting [hour]
-  (cond
-    (or (< hour 6) (>= hour 20)) "¡Buenas noches "
-    (<= 6 hour 11) "¡Buenos días "
-    (<= 12 hour 19) "¡Buenas tardes "))
+(defn greeting [name]
+  (str
+   (let [hour (get-hour-now)]
+     (cond
+       (or (< hour 6) (>= hour 20)) "¡Buenas noches "
+       (<= 6 hour 11) "¡Buenos días "
+       (<= 12 hour 19) "¡Buenas tardes "))
+   name
+   "!"))
 
 (defn main [& args]
   (let [name (first args)]
-    (println (str (greeting (get-hour-now)) name "!"))
+    (println (greeting name))
     (loop [line (read-line)]
       (if (= "Stop!" line)
         (println (str "Adios " name))
